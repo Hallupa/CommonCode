c1f5cce7ff98733d4762c8aee750e849801e608f
Hallupa <328720+Hallupa@users.noreply.github.com>
2020-07-18 22:15:23 +0100

    Split up view models Add single trade chart view

--- Src/TraderTools.Core.UI/ViewModels/DoubleChartViewModel.cs
+++ Src/TraderTools.Core.UI/ViewModels/DoubleChartViewModel.cs
@@ -1,214 +1,95 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
-using System.Linq;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
 using Abt.Controls.SciChart;
-using Abt.Controls.SciChart.Visuals.Annotations;
 using Hallupa.Library;
 using TraderTools.Basics;
 using TraderTools.Core.UI.Services;
-using TraderTools.Indicators;
 
 namespace TraderTools.Core.UI.ViewModels
 {
-    public abstract class DoubleChartViewModel : DependencyObject
+    public class DoubleChartViewModel : DependencyObject
     {
-        private Timeframe _largeChartTimeframe = Timeframe.H2;
-        private int _selectedMainIndicatorsIndex;
-        private Dispatcher _dispatcher;
 
         public DoubleChartViewModel()
         {
-            _dispatcher = Dispatcher.CurrentDispatcher;
             DependencyContainer.ComposeParts(this);
-
-            ChartViewModel.XVisibleRange = new IndexRange();
-            ChartViewModelSmaller1.XVisibleRange = new IndexRange();
-
-            LargeChartTimeframeOptions.Add(Timeframe.D1);
-            LargeChartTimeframeOptions.Add(Timeframe.H4);
-            LargeChartTimeframeOptions.Add(Timeframe.H2);
-            LargeChartTimeframeOptions.Add(Timeframe.H1);
-            LargeChartTimeframeOptions.Add(Timeframe.M30);
-            LargeChartTimeframeOptions.Add(Timeframe.M15);
-            LargeChartTimeframeOptions.Add(Timeframe.M5);
-            LargeChartTimeframeOptions.Add(Timeframe.M1);
 
             RemoveSelectedLineCommand = new DelegateCommand(t => RemoveSelectedLine());
         }
 
         public GridLength SmallerChartWidth { get; set; } = new GridLength(1, GridUnitType.Star);
 
-        public ChartViewModel ChartViewModel { get; } = new ChartViewModel();
+        public ChartViewModel RightChartViewModel { get; } = new ChartViewModel();
 
         [Import] public ChartingService ChartingService { get; private set; }
 
-        public ChartViewModel ChartViewModelSmaller1 { get; } = new ChartViewModel();
+        public ChartViewModel LeftChartViewModel { get; } = new ChartViewModel();
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public Timeframe SmallChartTimeframe { get; set; } = Timeframe.D1;
-        public ObservableCollection<Timeframe> LargeChartTimeframeOptions { get; } = new ObservableCollection<Timeframe>();
-
         public DelegateCommand RemoveSelectedLineCommand { get; private set; }
-        public Timeframe LargeChartTimeframe
-        {
-            get => _largeChartTimeframe;
-            set
-            {
-                if (_largeChartTimeframe == value) return;
-
-                _largeChartTimeframe = value;
-                SelectedLargeChartTimeframeIndex = LargeChartTimeframeOptions.IndexOf(value);
-
-                LargeChartTimeframeChanged();
-            }
-        }
-
-        public static readonly DependencyProperty SelectedLargeChartTimeframeIndexProperty = DependencyProperty.Register(
-            "SelectedLargeChartTimeframeIndex", typeof(int), typeof(DoubleChartViewModel), new PropertyMetadata(default(int)));
-
-        public int SelectedLargeChartTimeframeIndex
-        {
-            get => (int)GetValue(SelectedLargeChartTimeframeIndexProperty);
-            set
-            {
-                _dispatcher.Invoke(() =>
-                {
-                    SetValue(SelectedLargeChartTimeframeIndexProperty, value);
-                    if (LargeChartTimeframe != LargeChartTimeframeOptions[value])
-                    {
-                        LargeChartTimeframe = LargeChartTimeframeOptions[value];
-                    }
-                });
-            }
-        }
-
-        public int SelectedMainIndicatorsIndex
-        {
-            get => _selectedMainIndicatorsIndex;
-            set => _selectedMainIndicatorsIndex = value;
-        }
-
-        protected virtual void LargeChartTimeframeChanged()
-        {
-        }
 
         private void RemoveSelectedLine()
         {
-            if (ChartViewModel != null && ChartViewModel.ChartPaneViewModels.Count > 0 && ChartViewModel.ChartPaneViewModels[0].TradeAnnotations != null)
+            var toRemoveList = RightChartViewModel.GetSelectedLines();
+            if (toRemoveList != null)
             {
-                var toRemoveList = ChartViewModel.ChartPaneViewModels[0].TradeAnnotations.OfType<LineAnnotation>().Where(x => x.Tag is string s && s.StartsWith("Added") && x.IsSelected).ToList();
                 foreach (var toRemove in toRemoveList)
                 {
-                    ChartViewModel.ChartPaneViewModels[0].TradeAnnotations.Remove(toRemove);
+                    RightChartViewModel.ChartPaneViewModels[0].TradeAnnotations.Remove(toRemove);
 
-                    var linked = ChartViewModelSmaller1.ChartPaneViewModels[0].TradeAnnotations.OfType<LineAnnotation>().FirstOrDefault(x => x.Tag is string s && s.Equals((string)toRemove.Tag));
+                    var linked = LeftChartViewModel.GetLines((string)toRemove.Tag);
                     if (linked != null)
                     {
-                        ChartViewModelSmaller1.ChartPaneViewModels[0].TradeAnnotations.Remove(linked);
+                        foreach (var l in linked)
+                        {
+                            LeftChartViewModel.ChartPaneViewModels[0].TradeAnnotations.Remove(l);
+                        }
                     }
                 }
             }
 
-            if (ChartViewModelSmaller1 != null && ChartViewModelSmaller1.ChartPaneViewModels.Count > 0 && ChartViewModelSmaller1.ChartPaneViewModels[0].TradeAnnotations != null)
+            toRemoveList = LeftChartViewModel.GetSelectedLines();
+            if (toRemoveList != null)
             {
-                var toRemoveList = ChartViewModelSmaller1.ChartPaneViewModels[0].TradeAnnotations.OfType<LineAnnotation>().Where(x => x.Tag is string s && s.StartsWith("Added") && x.IsSelected).ToList();
                 foreach (var toRemove in toRemoveList)
                 {
-                    ChartViewModelSmaller1.ChartPaneViewModels[0].TradeAnnotations.Remove(toRemove);
+                    LeftChartViewModel.ChartPaneViewModels[0].TradeAnnotations.Remove(toRemove);
 
-                    if (ChartViewModel != null)
+                    var linked = RightChartViewModel.GetLines((string)toRemove.Tag);
+                    if (linked != null)
                     {
-                        var linked = ChartViewModel.ChartPaneViewModels[0].TradeAnnotations.OfType<LineAnnotation>()
-                            .FirstOrDefault(x => x.Tag is string s && s.Equals((string)toRemove.Tag));
-                        if (linked != null)
+                        foreach (var l in linked)
                         {
-                            ChartViewModel.ChartPaneViewModels[0].TradeAnnotations.Remove(linked);
+                            RightChartViewModel.ChartPaneViewModels[0].TradeAnnotations.Remove(l);
                         }
                     }
                 }
             }
         }
 
-
-        protected void ViewCandles(string market, Timeframe smallChartTimeframe, List<Candle> smallChartCandles,
-            Timeframe largeChartTimeframe, List<Candle> largeChartCandles)
+        /*protected void ShowTrade(Trade trade, Timeframe leftChartTimeframe, Timeframe rightChartTimeframe)
         {
-            LargeChartTimeframe = largeChartTimeframe;
-            ChartHelper.SetChartViewModelPriceData(largeChartCandles, ChartViewModel);
-            ChartHelper.SetChartViewModelIndicatorPaneData(largeChartCandles, ChartViewModel, new AverageTrueRange());
-
-            if (SelectedMainIndicatorsIndex == (int)MainIndicators.EMA8_EMA25_EMA50)
+            _dispatcher.BeginInvoke((Action)(() =>
             {
-                ChartHelper.AddIndicator(ChartViewModel.ChartPaneViewModels[0], market,
-                    new ExponentialMovingAverage(8), Colors.DarkBlue, largeChartTimeframe, largeChartCandles);
-               // ChartHelper.AddIndicator(ChartViewModel.ChartPaneViewModels[0], market,
-               //     new ExponentialMovingAverage(25), Colors.Blue, largeChartTimeframe, largeChartCandles);
-                ChartHelper.AddIndicator(ChartViewModel.ChartPaneViewModels[0], market,
-                    new ExponentialMovingAverage(50), Colors.SteelBlue, largeChartTimeframe, largeChartCandles);
-            }
-            else if (SelectedMainIndicatorsIndex == (int)MainIndicators.EMA20_MA50_MA200)
-            {
-                //ChartHelper.AddIndicator(ChartViewModel.ChartPaneViewModels[0], market,
-                //    new ExponentialMovingAverage(8), Colors.DarkBlue, largeChartTimeframe, largeChartCandles);
-                //ChartHelper.AddIndicator(ChartViewModel.ChartPaneViewModels[0], market,
-                //    new SimpleMovingAverage(50), Colors.Blue, largeChartTimeframe, largeChartCandles);
-               // ChartHelper.AddIndicator(ChartViewModel.ChartPaneViewModels[0], market,
-               //     new SimpleMovingAverage(200), Colors.SteelBlue, largeChartTimeframe, largeChartCandles);
-            }
+                LeftChartViewModel.ShowTrade(trade, leftChartTimeframe, false, s => { }, );
+                RightChartViewModel.ShowTrade(trade, rightChartTimeframe, false, s => { }, );
+            }));
+        }*/
 
-            ChartHelper.SetChartViewModelPriceData(smallChartCandles, ChartViewModelSmaller1);
-            ChartHelper.SetChartViewModelIndicatorPaneData(smallChartCandles, ChartViewModelSmaller1, new AverageTrueRange());
-
-            if (SelectedMainIndicatorsIndex == (int)MainIndicators.EMA8_EMA25_EMA50)
-            {
-                ChartHelper.AddIndicator(ChartViewModelSmaller1.ChartPaneViewModels[0], market,
-                    new ExponentialMovingAverage(8), Colors.DarkBlue, smallChartTimeframe, smallChartCandles);
-                // ChartHelper.AddIndicator(ChartViewModelSmaller1.ChartPaneViewModels[0], market,
-                //    new ExponentialMovingAverage(25), Colors.Blue, smallChartTimeframe, smallChartCandles);
-                ChartHelper.AddIndicator(ChartViewModelSmaller1.ChartPaneViewModels[0], market,
-                    new ExponentialMovingAverage(50), Colors.LightBlue, smallChartTimeframe, smallChartCandles);
-            }
-            else if (SelectedMainIndicatorsIndex == (int)MainIndicators.EMA20_MA50_MA200)
-            {
-                /*ChartHelper.AddIndicator(ChartViewModelSmaller1.ChartPaneViewModels[0], market,
-                    new ExponentialMovingAverage(20), Colors.DarkBlue, smallChartTimeframe, smallChartCandles);
-                ChartHelper.AddIndicator(ChartViewModelSmaller1.ChartPaneViewModels[0], market,
-                    new SimpleMovingAverage(50), Colors.Blue, smallChartTimeframe, smallChartCandles);
-                ChartHelper.AddIndicator(ChartViewModelSmaller1.ChartPaneViewModels[0], market,
-                    new SimpleMovingAverage(200), Colors.LightBlue, smallChartTimeframe, smallChartCandles);*/
-            }
-
-
-            ChartHelper.AddIndicator(ChartViewModel.ChartPaneViewModels[0], market,
-                 new BollingerBand(1.5F, 20), Colors.Green, largeChartTimeframe, largeChartCandles);
-
-            ChartHelper.AddIndicator(ChartViewModel.ChartPaneViewModels[0], market,
-                new BollingerBand(-1.5F, 20), Colors.Green, largeChartTimeframe, largeChartCandles, false); // Only need one entry in legend
-
-
-            if (ChartViewModel.ChartPaneViewModels[0].TradeAnnotations == null)
-            {
-                ChartViewModel.ChartPaneViewModels[0].TradeAnnotations = new AnnotationCollection();
-            }
-            else
-            {
-                ChartViewModel.ChartPaneViewModels[0].TradeAnnotations.Clear();
-            }
-
-            if (ChartViewModelSmaller1.ChartPaneViewModels[0].TradeAnnotations == null)
-            {
-                ChartViewModelSmaller1.ChartPaneViewModels[0].TradeAnnotations = new AnnotationCollection();
-            }
-            else
-            {
-                ChartViewModelSmaller1.ChartPaneViewModels[0].TradeAnnotations.Clear();
-            }
+        protected void ViewCandles(string market, Timeframe leftChartTimeframe, List<Candle> leftChartCandles,
+            Timeframe rightChartTimeframe, List<Candle> rightChartCandles,
+            List<(IIndicator Indicator, Color Color, bool ShowInLegend)> smallChartIndicators,
+            List<(IIndicator Indicator, Color Color, bool ShowInLegend)> largeChartIndicators)
+        {
+            RightChartViewModel.ViewCandles(market, rightChartTimeframe, rightChartCandles, largeChartIndicators);
+            LeftChartViewModel.ViewCandles(market, leftChartTimeframe, leftChartCandles, smallChartIndicators);
         }
     }
 }
---
+++ Src/TraderTools.Core.UI/ViewModels/TradeListViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Hallupa.Library.UI;
+using TraderTools.Basics;
+using TraderTools.Core.UI.Views;
+
+namespace TraderTools.Core.UI.ViewModels
+{
+    public class TradeListViewModel : DependencyObject, INotifyPropertyChanged
+    {
+        public TradeListViewModel()
+        {
+            TimeFrameItems = new List<Timeframe>
+            {
+                Timeframe.D1,
+                Timeframe.H4,
+                Timeframe.H2,
+                Timeframe.H1,
+                Timeframe.M30,
+                Timeframe.M15,
+                Timeframe.M5,
+                Timeframe.M1
+            };
+
+            TradesView = (CollectionView)CollectionViewSource.GetDefaultView(Trades);
+            TradesView.Filter = TradesViewFilter;
+        }
+
+        public CollectionView TradesView { get; private set; }
+
+        public List<Timeframe> TimeFrameItems { get; set; }
+
+        public TradeListDisplayOptionsFlag TradeListDisplayOptions { get; set; } =
+            TradeListDisplayOptionsFlag.PoundsPerPip | TradeListDisplayOptionsFlag.Stop
+                                                     | TradeListDisplayOptionsFlag.Limit
+                                                     | TradeListDisplayOptionsFlag.OrderPrice
+                                                     | TradeListDisplayOptionsFlag.OrderDate
+                                                     | TradeListDisplayOptionsFlag.Comments
+                                                     | TradeListDisplayOptionsFlag.ResultR
+                                                     | TradeListDisplayOptionsFlag.Broker
+                                                     | TradeListDisplayOptionsFlag.Timeframe
+                                                     | TradeListDisplayOptionsFlag.Strategies
+                                                     | TradeListDisplayOptionsFlag.Risk
+                                                     | TradeListDisplayOptionsFlag.Status
+                                                     | TradeListDisplayOptionsFlag.ClosePrice
+                                                     | TradeListDisplayOptionsFlag.Dates
+                                                     | TradeListDisplayOptionsFlag.Profit;
+
+        public ObservableCollectionEx<Trade> Trades { get; } = new ObservableCollectionEx<Trade>();
+
+        private bool TradesViewFilter(object obj)
+        {
+            var t = (Trade)obj;
+            return ((ShowOpenTrades && t.EntryPrice != null && t.CloseDateTime == null)
+                   || (ShowOrders && t.OrderPrice != null && t.EntryPrice == null && t.CloseDateTime == null)
+                   || (ShowClosedTrades && t.CloseDateTime != null));
+        }
+
+        public static readonly DependencyProperty TradeSelectionModeProperty = DependencyProperty.Register(
+            "TradeSelectionMode", typeof(DataGridSelectionMode), typeof(TradeListViewModel), new PropertyMetadata(DataGridSelectionMode.Extended));
+
+        public DataGridSelectionMode TradeSelectionMode
+        {
+            get { return (DataGridSelectionMode) GetValue(TradeSelectionModeProperty); }
+            set { SetValue(TradeSelectionModeProperty, value); }
+        }
+        public Trade SelectedTrade
+        {
+            get => _selectedTrade;
+            set
+            {
+                _selectedTrade = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public static readonly DependencyProperty ShowOpenTradesProperty = DependencyProperty.Register("ShowOpenTrades", typeof(bool), typeof(TradeListViewModel), new PropertyMetadata(true, ShowTradesChanged));
+        public static readonly DependencyProperty ShowClosedTradesProperty = DependencyProperty.Register("ShowClosedTrades", typeof(bool), typeof(TradeListViewModel), new PropertyMetadata(true, ShowTradesChanged));
+        public static readonly DependencyProperty ShowOrdersProperty = DependencyProperty.Register("ShowOrders", typeof(bool), typeof(TradeListViewModel), new PropertyMetadata(true, ShowTradesChanged));
+
+        private static void ShowTradesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var tvm = (TradeListViewModel)d;
+            tvm.TradesView.Refresh();
+        }
+
+        public bool ShowOpenTrades
+        {
+            get { return (bool)GetValue(ShowOpenTradesProperty); }
+            set { SetValue(ShowOpenTradesProperty, value); }
+        }
+
+        public bool ShowClosedTrades
+        {
+            get { return (bool)GetValue(ShowClosedTradesProperty); }
+            set { SetValue(ShowClosedTradesProperty, value); }
+        }
+
+        public bool ShowOrders
+        {
+            get { return (bool)GetValue(ShowOrdersProperty); }
+            set { SetValue(ShowOrdersProperty, value); }
+        }
+
+        private Window _parent;
+        private Trade _selectedTrade;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
